2b90f602b2949600a35114d487efc0b79806427e
Leandro Daniel Correia Contreras <leandro.contreras@tecnico.ulisboa.pt>
2021-02-19 16:11:05 +0000

    started footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <footer>
+    <p class="p--small--caps">
+      © {{ currentYear }} Leandro Contreras. Designed and developed by yours
+      truly.
+    </p>
+    <div></div>
+  </footer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      currentYear: new Date().getFullYear()
+    };
+  }
+};
+</script>

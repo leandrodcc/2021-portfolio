62a74d59e3bc7bd43e0066a85608365f37cac3f0
Leandro Daniel Correia Contreras <leandro.contreras@tecnico.ulisboa.pt>
2021-02-21 18:06:41 +0000

    working on global pink transition

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <RouterView />
+    <transition name="main-transition" mode="out-in">
+      <RouterView />
+    </transition>
   </div>
 </template>
 
@@ -11,4 +13,32 @@
 @import "@/assets/scss/_typography.scss";
 @import "@/assets/scss/_grid.scss";
 @import "@/assets/scss/_tags.scss";
+
+.main-transition-enter-active,
+.main-transition-leave-active {
+  transition: all 0.5s;
+}
+
+.main-transition-enter,
+.main-transition-leave-to {
+  opacity: 0;
+}
+
+// .main-transition-enter,
+// .main-transition-leave-to {
+//   position: relative;
+//   opacity: 0;
+//   background-color: $pink;
+
+//   &::after {
+//     content: "";
+//     position: absolute;
+//     top: 0;
+//     left: 0;
+//     width: 100%;
+//     height: 100vh;
+//     z-index: 99;
+//     background-color: $pink;
+//   }
+// }
 </style>
